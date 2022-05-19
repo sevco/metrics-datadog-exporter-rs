d66b624270b82de32188dcad9c4e8bcd36255f48
Aaron Griffin <aaron@sevco.io>
2022-05-19 12:39:54 -0700

    Break large requests up

--- src/builder.rs
+++ src/builder.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 
 use metrics::Label;
-use metrics_util::Registry;
+use metrics_util::registry::{AtomicStorage, Registry};
 use reqwest::Client;
 
 use crate::exporter::DataDogExporter;
@@ -65,7 +65,7 @@ impl DataDogBuilder {
 
     /// Build [`DataDogHandle`]
     pub fn build(&self) -> DataDogHandle {
-        let registry = Arc::new(Registry::new());
+        let registry = Arc::new(Registry::new(AtomicStorage));
         let recorder = DataDogRecorder::new(registry.clone());
         let handle = DataDogExporter::new(
             registry,
